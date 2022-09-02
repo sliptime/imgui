4c1d5aceedfb9a0443400b6e7d72feac258858db
Tom Xiong <9060084+Txion001@users.noreply.github.com>
2022-09-01 23:48:35 -0700

    Changes to be committed: 	modified:   ImGui.vcxproj 	modified:   bin-int/Debug-windows-x86_64/ImGui/ImGui.tlog/CL.command.1.tlog 	modified:   bin-int/Debug-windows-x86_64/ImGui/ImGui.tlog/CL.write.1.tlog 	modified:   bin/Debug-windows-x86_64/ImGui/ImGui.lib 	modified:   premake5.lua
     Untracked files:
    	bin/Debug-windows-x86_64/ImGui/ImGui.idb

--- premake5.lua
+++ premake5.lua
@@ -1,31 +1,34 @@
 project "ImGui"
   kind "StaticLib"
   language "C++"
+  cppdialect "C++17"
+  staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-    "imconfig.h",
-    "imgui.cpp",
-    "imgui.h",
-    "imgui_demo.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_tables.cpp",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h"
+	"imconfig.h",
+	"imgui.cpp",
+	"imgui.h",
+	"imgui_demo.cpp",
+	"imgui_draw.cpp",
+	"imgui_internal.h",
+	"imgui_tables.cpp",
+	"imgui_widgets.cpp",
+	"imstb_rectpack.h",
+	"imstb_textedit.h",
+	"imstb_truetype.h"
 	}
-
-    defines { "IMGUI_API=__declspec(dllexport)" }
 
 	filter "system:windows"
 		systemversion "latest"
-    cppdialect "C++17"
-		staticruntime "On"
 
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		symbols "on"
